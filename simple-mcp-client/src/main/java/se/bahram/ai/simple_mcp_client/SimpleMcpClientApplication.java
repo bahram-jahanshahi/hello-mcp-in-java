133a6e09f916fd81dac184b3147ca7f5a40e5997
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-09-18 15:07:44 +0200

    Init

---
+++ simple-mcp-client/src/main/java/se/bahram/ai/simple_mcp_client/SimpleMcpClientApplication.java
@@ -0,0 +1,13 @@
+package se.bahram.ai.simple_mcp_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMcpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMcpClientApplication.class, args);
+	}
+
+}
